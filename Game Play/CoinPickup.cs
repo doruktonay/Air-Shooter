1d8587246eca8660337597eed34fd6335244f262
tonay doruk <47057052+doruktonay@users.noreply.github.com>
2025-05-15 01:16:02 +0300

    some scripts for my game projects

---
+++ Game Play/CoinPickup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class CoinPickup : MonoBehaviour
+{
+    public UnityEvent onPickedUp;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Coin"))
+        {
+            PoolingManager.instance.ReturnObject(other.gameObject);
+            onPickedUp.Invoke();
+
+            StatsManager.instance.AddSceneMoney(1);
+        }
+    }
+}
